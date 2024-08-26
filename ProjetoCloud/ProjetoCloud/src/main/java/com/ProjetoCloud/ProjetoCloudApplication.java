a4b0db351392267d1f9af96f73c38e52b3f4a204
Rafael Lima <105614039+rafaelf-lima@users.noreply.github.com>
2024-08-26 08:57:31 -0300

    Projeto Cloud (Spring)

---
+++ ProjetoCloud/ProjetoCloud/src/main/java/com/ProjetoCloud/ProjetoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.ProjetoCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCloudApplication.class, args);
+	}
+
+}
---
+++ ProjetoCloud/ProjetoCloud/src/test/java/com/ProjetoCloud/ProjetoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjetoCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
